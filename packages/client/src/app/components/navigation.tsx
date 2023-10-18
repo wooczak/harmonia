59e7a4a4d2fb3f7c5643494198e8b280b0448cd8
Kacper Luczak <kacper.luczak99@gmail.com>
2023-10-18 17:50:57 +0200

    Creating first client-side UI with MUI

--- packages/client/src/app/components/navigation.tsx
+++ packages/client/src/app/components/navigation.tsx
@@ -1,11 +1,26 @@
+import { AppBar, Container, IconButton, Tooltip, Typography } from '@mui/material'
 import React from 'react'
+import MenuIcon from '@mui/icons-material/Menu'
 
 type TNavigationProps = {
-  tabs: string[];
+  tabs: string[]
 }
 
 export default function Navigation({ tabs }: TNavigationProps) {
   return (
-    <nav>{tabs?.map(tab => <li className="text-3xl">{tab}</li>)}</nav>
+    <AppBar>
+      <Container maxWidth="xl">
+        <Tooltip title="Menu" placement="left">
+          <IconButton>
+            <MenuIcon />
+          </IconButton>
+        </Tooltip>
+        {tabs?.map((tab, index) => (
+          <Typography key={index} component="a">
+            {tab}
+          </Typography>
+        ))}
+      </Container>
+    </AppBar>
   )
 }
--- packages/client/src/app/layout.tsx
+++ packages/client/src/app/layout.tsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import Navigation from './components/navigation';
-import { NavigationTabs } from './constants';
+import React from 'react'
+import Navigation from './components/navigation'
+import { NavigationTabs } from './constants'
+import './globals.css'
+import ThemeRegistry from './ThemeRegistry'
 
-export default function RootLayout({
-  children
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
-        <Navigation tabs={NavigationTabs}/>
-        {children}
+        <ThemeRegistry options={{ key: 'mui' }}>
+          <Navigation tabs={NavigationTabs} />
+          {children}
+        </ThemeRegistry>
       </body>
     </html>
   )
