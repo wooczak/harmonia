bb27b7602ec695034c945370a58d1ed52498c876
Kacper Luczak <kacper.luczak99@gmail.com>
2023-10-29 17:58:21 +0100

    Finishing and succesfully running the first test

--- packages/client/__tests__/organisms/Navigation/helpers.test.tsx
+++ packages/client/__tests__/organisms/Navigation/helpers.test.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import { render } from '@testing-library/react'
 import { renderTabs } from 'src/components/organisms/Navigation/helpers'
 
@@ -11,8 +12,13 @@ describe('WHEN the renderTabs receives tabs as argument', function () {
   it('THEN renders them as <li/> elements', function () {
     const { getByText, getAllByRole } = render(<ul>{renderTabs(mockTabs)}</ul>)
 
+    const links = getAllByRole('link');
+    const firstRoute = (links[0] as HTMLLinkElement).href;
+    const secondRoute = (links[1] as HTMLLinkElement).href;
+
     expect(getByText('Go to test route one')).toBeDefined();
     expect(getByText('Go to test route two')).toBeDefined();
-    
+    expect(firstRoute).toBe("http://test-environment.test/test-route-one");
+    expect(secondRoute).toBe("http://test-environment.test/test-route-two");
   })
 })
--- packages/client/jest.config.js
+++ packages/client/jest.config.js
@@ -1,3 +1,29 @@
 module.exports = {
-  testEnvironment: 'jsdom',
+  transform: {
+    // Running Jest with Next.js' SWC
+    "^.+\\.(t|j)(s|sx)?$": [
+      "@swc/jest", 
+      { jsc: {
+        transform: {
+          react: {
+            runtime: 'automatic',
+          },
+        },
+      },
+    },
+  ]
+  },
+    moduleFileExtensions: ['js', 'json', 'ts', 'jsx', 'tsx'],
+    roots: ['__tests__'],
+    collectCoverageFrom: ['src/**/*.(j|t)(s|sx)'],
+    coverageDirectory: './coverage',
+    testEnvironment: 'jsdom',  
+    testEnvironmentOptions: {
+      url: 'http://test-environment.test/',
+    },
+    moduleDirectories: ['node_modules', 'src'],
+    moduleNameMapper: {
+      "^src/(.*)$": "<rootDir>/src/$1"
+    },
+    modulePaths: ["<rootDir>/src"]
 };
